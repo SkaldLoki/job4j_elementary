468d43dc6469507c36f3c2bd61fca4e6d0ef10b0
SkaldLoki <skaldloki@gmail.com>
2023-07-09 23:40:54 +1000

    4.1. Разбор первой программы (Multiple). [#159033 #36528]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Скальд Локи");
+        System.out.println("16.02.1994");
+    }
+}
